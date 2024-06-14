347ccc5598e24756bf6423ff97a398a82e5d6927
IamGOOD <e.rukshenas@gmail.com>
2024-06-13 23:09:18 -0400

    Static files added; crispy forms added; sign up and log in changed

--- Books/config/settings.py
+++ Books/config/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     # local
     'accounts',
     'pages',
+    # 3rd
+    'crispy_bootstrap4',
+    'crispy_forms',
 ]
+
+# django-crispy-forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -122,7 +128,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
+
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
+
+STATICFILES_FINDERS = [
+"django.contrib.staticfiles.finders.FileSystemFinder",
+"django.contrib.staticfiles.finders.AppDirectoriesFinder",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -133,4 +148,3 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
-

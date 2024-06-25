4b6c62ac14f2ccbe7b673c1802295e2073545345
IamGOOD <e.rukshenas@gmail.com>
2024-06-24 22:08:28 -0400

    Using allouth_account was updated lig in and log out procceses

--- Books/config/settings.py
+++ Books/config/settings.py
@@ -156,10 +156,12 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 ACCOUNT_LOGOUT_REDIRECT = 'home'
 SITE_ID = 1
+ACCOUNT_SESSION_REMEMBER = True
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
+ACCOUNT_SIGNUP_PASSWORD_ENTER_TWICE = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
